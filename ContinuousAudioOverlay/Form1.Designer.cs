3f4c4970186b11e355090874d4ae60db85892601
Josef Micak <micak.josef@email.cz>
2023-10-31 22:01:34 +0100

    Initial version of the app
    Added initial version of the app. Includes many of the most important features, such as internet radio stream, pause/skip songs, volume control and audio output device control.

---
+++ ContinuousAudioOverlay/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace ContinuousAudioOverlay
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(245, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Čas Rock";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(2, 2);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(233, 77);
+            this.axWindowsMediaPlayer1.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(245, 40);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Rádio Kiss";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button3.Location = new System.Drawing.Point(83, 85);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(152, 46);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Pause/Play";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button5.Location = new System.Drawing.Point(245, 85);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 46);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Next";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button6.Location = new System.Drawing.Point(2, 85);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 46);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Prev";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(2, 137);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(318, 45);
+            this.trackBar1.TabIndex = 7;
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(143, 174);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "label1";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(2, 200);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(318, 21);
+            this.comboBox1.TabIndex = 9;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(327, 226);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TopMost = true;
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ ContinuousAudioOverlay/Form1.cs
@@ -0,0 +1,178 @@
+﻿using AudioSwitcher.AudioApi;
+using AudioSwitcher.AudioApi.CoreAudio;
+using NAudio.CoreAudioApi;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.WebRequestMethods;
+
+namespace ContinuousAudioOverlay
+{
+    public partial class Form1 : Form
+    {
+        CoreAudioDevice defaultPlaybackDevice = new CoreAudioController().DefaultPlaybackDevice;
+        IEnumerable<CoreAudioDevice> devices = new CoreAudioController().GetPlaybackDevices();
+
+        public Form1()
+        {
+            InitializeComponent();
+            trackBar1.Value = (int)defaultPlaybackDevice.Volume;
+            InitializeOutputDevices();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "https://icecast9.play.cz/casrock192.mp3";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "http://icecast4.play.cz/kiss128.mp3";
+        }
+
+        public enum AppCommands
+        {
+            BrowserBack = 1,
+            BrowserForward = 2,
+            BrowserRefresh = 3,
+            BrowserStop = 4,
+            BrowserSearch = 5,
+            BrowserFavorite = 6,
+            BrowserHome = 7,
+            VolumeMute = 8,
+            VolumeDown = 9,
+            VolumeUp = 10,
+            MediaNext = 11,
+            MediaPrevious = 12,
+            MediaStop = 13,
+            MediaPlayPause = 14,
+            LaunchMail = 15,
+            LaunchMediaSelect = 16,
+            LaunchApp1 = 17,
+            LaunchApp2 = 18,
+            BassDown = 19,
+            BassBoost = 20,
+            BassUp = 21,
+            TrebleUp = 22,
+            TrebleDown = 23,
+            MicrophoneMute = 24,
+            MicrophoneVolumeUp = 25,
+            MicrophoneVolumeDown = 26,
+            Help = 27,
+            Find = 28,
+            New = 29,
+            Open = 30,
+            Close = 31,
+            Save = 32,
+            Print = 33,
+            Undo = 34,
+            Redo = 35,
+            Copy = 36,
+            Cut = 37,
+            Paste = 38,
+            ReplyToMail = 39,
+            ForwardMail = 40,
+            SendMail = 41,
+            SpellCheck = 42,
+            Dictate = 43,
+            MicrophoneOnOff = 44,
+            CorrectionList = 45,
+            MediaPlay = 46,
+            MediaPause = 47,
+            MediaRecord = 48,
+            MediaFastForward = 49,
+            MediaRewind = 50,
+            MediaChannelUp = 51,
+            MediaChannelDown = 52,
+            Delete = 53,
+            Flip3D = 54
+        }
+
+        public static void Send(AppCommands cmd)
+        {
+            if (frm == null) Initialize();
+            frm.Invoke(new MethodInvoker(() => SendMessage(frm.Handle, WM_APPCOMMAND, frm.Handle, (IntPtr)((int)cmd << 16))));
+        }
+
+        private static void Initialize()
+        {
+            // Run the message loop on another thread so we're compatible with a console mode app
+            var t = new Thread(() => {
+                frm = new Form();
+                var dummy = frm.Handle;
+                frm.BeginInvoke(new MethodInvoker(() => mre.Set()));
+                Application.Run();
+            });
+            t.SetApartmentState(ApartmentState.STA);
+            t.IsBackground = true;
+            t.Start();
+            mre.WaitOne();
+        }
+
+        private static ManualResetEvent mre = new ManualResetEvent(false);
+        private static Form frm;
+
+        // Pinvoke
+        private const int WM_APPCOMMAND = 0x319;
+        [DllImport("user32.dll")]
+        private static extern IntPtr SendMessage(IntPtr hWnd, int msg, IntPtr wp, IntPtr lp);
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Send(AppCommands.MediaPlayPause);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Send(AppCommands.MediaPrevious);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Send(AppCommands.MediaNext);
+        }
+
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            defaultPlaybackDevice.Volume = trackBar1.Value;
+            label1.Text = trackBar1.Value.ToString();
+        }
+
+        public void InitializeOutputDevices()
+        {
+            foreach (CoreAudioDevice d in devices)
+            {
+                if(d.Name == "Speakers" || d.Name == "Headphones")
+                {
+                    comboBox1.Items.Add(d.FullName);
+                }
+                if (d.IsDefaultDevice)
+                {
+                    comboBox1.SelectedIndex = comboBox1.Items.Count - 1;
+                }
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            foreach (CoreAudioDevice d in devices)
+            {
+                if (d.FullName == comboBox1.Text)
+                {
+                    d.SetAsDefault();
+                    defaultPlaybackDevice = d;
+                    trackBar1.Value = (int)d.Volume;
+                } 
+            }
+        }
+    }
+}
