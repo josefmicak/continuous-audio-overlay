0cb42e1e7c6c9bc47f797ec357578152fbc5858a
Josef Micak <micak.josef@email.cz>
2024-03-06 21:11:53 +0100

    TopMost native win32 call
    The TopMost property is now also set using a native win32 call on startup.

--- Form1.cs
+++ Form1.cs
@@ -44,6 +44,7 @@ namespace ContinuousAudioOverlay
             InitializeTimer();
             volumeSlider.Value = (int)defaultPlaybackDevice.Volume;
             this.TopMost = true;
+            SetWindowPos(this.Handle, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_SHOWWINDOW);
             UpdateSourceLabel("<no source>");
         }
 
@@ -745,5 +746,13 @@ namespace ContinuousAudioOverlay
                 SendMessage(Handle, WM_NCLBUTTONDOWN, HTCAPTION, 0);
             }
         }
+
+        [DllImport("user32.dll")]
+        static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
+        static readonly IntPtr HWND_TOPMOST = new IntPtr(-1);
+        const UInt32 SWP_NOSIZE = 0x0001;
+        const UInt32 SWP_NOMOVE = 0x0002;
+        const UInt32 SWP_SHOWWINDOW = 0x0040;
     }
 }
